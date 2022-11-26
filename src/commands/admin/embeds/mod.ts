9c0cfa2ccf98de4d5d907b7fb283e36c015c5187
kttykat <kat@saige.lgbt>
2022-11-27 00:22:26 +0100

    Prod ready

--- src/commands/admin/embeds/mod.ts
+++ src/commands/admin/embeds/mod.ts
@@ -7,8 +7,20 @@ export default new Command({
     category: 'admin',
     description: `Send out the general embeds! :nerd:`,
     main: (props) => {
-        ruleEmbeds(props.ctx)
-        ticketEmbed(props.ctx)
-        props.client.commands.get("rr").main(props)
+        const type = props.args && props.args.length > 0 ? props.args[0] : null
+        if(!type) {
+            ruleEmbeds(props.ctx)
+            ticketEmbed(props.ctx)
+            props.client.commands.get("rr").main(props)
+        } else {
+            switch (type.toLowerCase()) {
+                case "rr":
+                    return props.client.commands.get("rr").main(props)
+                case "tickets":
+                    return ticketEmbed(props.ctx)
+                case "rules": 
+                    return ruleEmbeds(props.ctx)
+            }
+        }
     },
 })
